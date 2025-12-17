cbea533d9d64ab1443f7fae2ccc8d0f41242a162
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-12-17 15:06:36 +0530

    track alert metadata

---
+++ internal/core/domain/alert_metadata.go
@@ -0,0 +1,17 @@
+package domain
+
+type AlertMetadata struct {
+	RuleID         string
+	RuleVersion    string
+	ModelVersion   string
+	FeatureSetHash string
+}
+
+type MitigationAction string
+
+const (
+	ActionThrottle MitigationAction = "throttle"
+	ActionDegrade  MitigationAction = "degrade"
+	ActionFailover MitigationAction = "failover"
+	ActionObserve  MitigationAction = "observe"
+)
