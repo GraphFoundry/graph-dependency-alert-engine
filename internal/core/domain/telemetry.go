dfcdc7e78d39332d203617c5b84634366c87a8e9
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-12-19 04:57:34 +0530

    expand telemetry models

--- internal/core/domain/telemetry.go
+++ internal/core/domain/telemetry.go
@@ -3,8 +3,12 @@ package domain
 import "time"
 
 type Telemetry struct {
-	Service      ServiceNode
-	LatencyP95Ms float64
-	ErrorRate    float64 // 0..1
-	Timestamp    time.Time
+	Service       ServiceNode
+	LatencyP95Ms  float64
+	LatencyP99Ms  float64
+	ErrorRate     float64 // instant/short-term
+	ErrorRate1m   float64
+	ErrorRate5m   float64
+	ThroughputRPS float64
+	Timestamp     time.Time
 }
